3238fa31e1b5b7225ebada845932429d18e2d62a
trios4504 <67927315+trios4504@users.noreply.github.com>
2021-01-20 20:10:55 -0700

    Add files via upload

---
+++ CS_133_Projects/CS_133_Projects/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CS_133_Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[10, 10];
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    myArray[x, y] = (x + 1) * (y + 1);
+                }
+            }
+
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    Console.Write(myArray[x, y] + " ");
+                }
+
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
